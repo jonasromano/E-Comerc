d14e76dfc8f2ef2631f48996421de69e9e56e9ff
jonasromano <88295040+jonasromano@users.noreply.github.com>
2021-08-18 17:00:17 -0300

    new
    basado en lo visto en clase

--- js/products.js
+++ js/products.js
@@ -1,9 +1,28 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
+var productes = [];
+
+function mostrarProductos(array){
+  let product ="<hr>";
+  for(let i = 0; i < array.length; i++){
+    let producto = array[i];
+
+    product += "Nombre:" +producto.name +"<br>";
+    product += "Descripcion:" +producto.description +"<br>";
+    product += "Precio:" +producto.cost + "<br>";
+
+  }
+  document.getElementById("lista").innerHTML = product;
+}
 
 document.addEventListener("DOMContentLoaded", function (e) {
-    getJSONData(PRODUCTS_URL).then(function(resultObj){ 
-     if ("https://japdevdep.github.io/ecommerce-api/product/all.json".status === "ok");
-   });
-})
+
+    getJSONData(PRODUCTS_URL).then(function(resultObj) { 
+     if (resultObj.status === "ok") {
+       productes = resultObj.data;
+       mostrarProductos(productes);
+     }
+      
+    });
+ });
