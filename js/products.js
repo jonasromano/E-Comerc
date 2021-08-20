f503b822e06e7f1180057ea1b3789366f03c5204
jonasromano <88295040+jonasromano@users.noreply.github.com>
2021-08-20 00:57:11 -0300

    Segunda parte
    Mejorando estilos en css y agregando el html para antes del incio

--- js/products.js
+++ js/products.js
@@ -29,7 +29,7 @@ function mostrarImagenes(){
           ((maxCount == undefined) || (maxCount != undefined && parseInt(img.productCount) <= maxCount))){
 
           imagenes += `
-          <a href="product-info.html" class="list-group-item list-group-item-action">
+          <a href="products.html" class="list-group-item list-group-item-action">
               <div class="row">
                   <div class="col-3">
                       <img src="` + img.imgSrc + `" alt="` + img.description + `" class="img-thumbnail">
