e6ae6ebb71f89fe64e4c4d24f03b93442021b3ac
jonasromano <88295040+jonasromano@users.noreply.github.com>
2021-08-20 10:06:09 -0300

    grupal
    pautas del trabajo grupal

--- js/login.js
+++ js/login.js
@@ -1,6 +1,38 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
 
+document.addEventListener("DOMContentLoaded", function(e){
+   document.getElementById("submitBtn");("click", function(){
+       let Contraseña = document.getElementById("Contraseña");
+       let Correo = document.getElementById("Correo");
+       let camposCompletos = true; 
+       
+       if(Contraseña.value === " "){
+           Contraseña.classList.add("invalid");
+           camposCompletos = false;
+       }else{
+           Contraseña.classList.remove("invalid");
+       }
+       if(Correo.value === " "){
+           Correo.classList.add("invalid");
+           camposCompletos = false;
+       }else{
+           Correo.classList.remove("invalid");
+       }
+       if(camposCompletos){
+           getJSONData().then(function(resultado){
+               if(resultado.status === "ok"){
+                   usersArray = resultado.data;
+                   if(validateUser(usersArray,Contraseña.value,Correo.value)){
+                       window.location = 'index.html';
+                   }else{
+                       alert("Usuario o contraseña incorrectas!");
+                   }
+               }
+           });
+       }else{
+           alert("Debes ingresar los datos!")
+       }
+   });
 });
