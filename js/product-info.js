8230c4b4b1c9840974f41166f4256fcafc35b327
jonasromano <88295040+jonasromano@users.noreply.github.com>
2021-09-13 13:29:24 -0300

    6

--- js/product-info.js
+++ js/product-info.js
@@ -1,35 +1,32 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-function verInfo(name){
-    localStorage.setItem("producto",JSON.stringify({imgname}));
-    window.location = "product-info.html";
-}
+var array=PRODUCT_INFO_URL;
+
 function mostrarInfo(array){
 
+    let img = array[localStorage.getItem('name')];
     let information = "";
-    for(let i = 0; i < array.length; i++){
-        let img = array[i];
-
-    }
+    
+    information += `
     information += "Nombre:"+img.name+"<br>";
     information += "Descripcion:"+img.description+"<br>";
     information += "Precio:"+"USD"+img.costo+"<br>";
     information += "Stock:"+img.soldCount+"<br>";
     information += "Categoria:"+img.categoría+"<br>";
     information += "Productos Relacionados:"+img.productosRelacionados+"<br>";
-     information += `<button style="float :right;" onclick="Mas Info(`+ img.name +`)">Mas Info</button><br>`
+    `
 }
-document.getElementById("infopr").innerHTML= information;
 
+document.getElementById("infopr").innerHTML=
 
 document.addEventListener("DOMContentLoaded", function (e) {
 
     getJSONData(PRODUCT_INFO_URL).then(function(resultObj) { 
      if (resultObj.status === "ok") {
-       productes = resultObj.data;
+       InfoArray= resultObj.data;
        
-       mostrarInfo(array);
+       mostrarInfo(InfoArray);
      }
       
     });
