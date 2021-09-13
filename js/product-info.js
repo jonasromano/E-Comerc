704d3bafbf32018527d3442ea9fd00b35b6e0826
jonasromano <88295040+jonasromano@users.noreply.github.com>
2021-09-13 16:20:21 -0300

    Intento 7
    La informacion e imagenes del producto aparecen una vez que esta en la pagina de info product, aun faltan algunos items y buscar que manera puedo hacer para que al hacer click en otro producto me muestre la informacion de ese producto

--- js/product-info.js
+++ js/product-info.js
@@ -1,34 +1,45 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-var array=PRODUCT_INFO_URL;
+var information = {};
 
 function mostrarInfo(array){
 
-    let img = array[localStorage.getItem('name')];
-    let information = "";
-    
-    information += `
-    information += "Nombre:"+img.name+"<br>";
-    information += "Descripcion:"+img.description+"<br>";
-    information += "Precio:"+"USD"+img.costo+"<br>";
-    information += "Stock:"+img.soldCount+"<br>";
-    information += "Categoria:"+img.categoría+"<br>";
-    information += "Productos Relacionados:"+img.productosRelacionados+"<br>";
-    `
+  let infop ="";
+  for(let i = 0 ; i < array.length; i++){
+    let  imageSrc = array[i];
+
+    infop +=`
+        <div class="col-lg-3 col-md-4 col-6">
+            <div class="d-block mb-4 h-100">
+                <img class="img-fluid img-thumbnail" src="` + imageSrc + `" alt="">
+            </div>
+        </div>
+        `
+  }
+  document.getElementById("img").innerHTML= infop;
+
 }
 
-document.getElementById("infopr").innerHTML=
+
 
 document.addEventListener("DOMContentLoaded", function (e) {
 
     getJSONData(PRODUCT_INFO_URL).then(function(resultObj) { 
      if (resultObj.status === "ok") {
-       InfoArray= resultObj.data;
-       
-       mostrarInfo(InfoArray);
+       information = resultObj.data;
+
+       let informationNameHTML = document.getElementById("informationName");
+       let descriptionHTML = document.getElementById("description");
+       let soldCountHTML = document.getElementById("soldCount");
+       let informationCostHTML = document.getElementById("informationCost");
+   
+       informationNameHTML.innerHTML = information.name;
+       descriptionHTML.innerHTML = information.description;
+       soldCountHTML.innerHTML = information.soldCount;
+       informationCostHTML.innerHTML = information.cost;
      }
-      
+     mostrarInfo(information.images);
     });
     
  });
