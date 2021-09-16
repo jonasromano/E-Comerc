fb7c17e9548e3ad9548c565685d9897a7b9a6a71
jonasromano <88295040+jonasromano@users.noreply.github.com>
2021-09-16 13:41:09 -0300

    e

--- js/product-info.js
+++ js/product-info.js
@@ -54,8 +54,8 @@ document.addEventListener("DOMContentLoaded", function (e) {
     let  comment = array[i];
 
     comentario +=`
-    <div class="fa fa-star checked">`+comment.score+`</div>
-    <div class="row container p-2 m-3">
+    <div id="stars" class="fa fa-star checked">`+comment.score+`</div>
+    <div id="y" class="row container p-2 m-3">
     <p id="x"class="row container p-2 m-3">`+comment.description+`</p></div>
     <p> USUARIO :`+comment.user+` <br> <div class="col text-right">Fecha: `+comment.dateTime+`</div></p>
     </div>
@@ -72,4 +72,44 @@ document.addEventListener("DOMContentLoaded",function(e){
     }
     mostrarComents(comments);
   })
+})
+
+function rating(){
+  var comment = document.getElementsByName("rating");
+  for(var i = 0;i< comment.length; i++){
+    if(comment[i].checked){
+      return parseInt(comment[i].value);
+    }
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function(e){
+  document.getElementById("stars").innerHTML=`
+  <div class="star-rating">
+    <input id="star-5" type="radio" name="rating" value="5" />
+    <label for="star-5" title="5 stars">
+    <i class="active fa fa-star"></i>
+    </label>
+
+    <input id="star-4" type="radio" name="rating" value="4" />
+    <label for="star-4" title="4 stars">
+    <i class="active fa fa-star"></i>
+    </label>
+
+    <input id="star-3" type="radio" name="rating" value="3" />
+    <label for="star-3" title="3 stars">
+    <i class="active fa fa-star"></i>
+    </label>
+
+    <input id="star-2" type="radio" name="rating" value="2" />
+    <label for="star-2" title="2 stars">
+    <i class="active fa fa-star"></i>
+    </label>
+
+    <input id="star-1" type="radio" name="rating" value="1" />
+    <label for="star-1" title="1 stars">
+    <i class="active fa fa-star"></i>
+    </label>
+  </div>
+  `
 })
