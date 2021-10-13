7091afc6fa67c7b3f8c09163790485dfdbfc418d
jonasromano <88295040+jonasromano@users.noreply.github.com>
2021-10-13 18:34:39 -0300

    j

--- js/cart.js
+++ js/cart.js
@@ -11,7 +11,7 @@ function mostrarCarrito(articles){
 
 
           carro += `
-          <div class="container"id="car">
+          <div class="card"id="car">
           <div class="row">
               <div class="col">
                       <div class="d-flex w-100 justify-content-between">
@@ -25,12 +25,6 @@ function mostrarCarrito(articles){
                           <big class="text">` + carri.count +` artículos</big><br>
                           <big class="text">` + carri.currency + carri.unitCost + `</big>
                       </div>
-              </div>
-              
-              <div class="col">
-              <p> Subtotal:</p>
-              <h1> Total:</h1>
-              <h2> Envio:</h2>
               </div>
               </div>
               
@@ -49,18 +43,6 @@ document.addEventListener("DOMContentLoaded", function (e) {
     getJSONData(CART_INFO_URL).then(function(resultObj) { 
      if (resultObj.status === "ok") {
        articles = resultObj.data;
-
-       let nameHTML= document.getElementById("name");
-       let countHTML= document.getElementById("count");
-       let unitCostHTML= document.getElementById("unitCost");
-
-       nameHTML.innerHTML= articles.name;
-       countHTML.innerHTML= articles.count;
-       unitCostHTML.innerHTML= articles.unitCost;
-
-
-       
-       
      }
      mostrarCarrito(articles.articles);
     });
