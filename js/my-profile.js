c84290326ee760ca1d342e2c5e7f805f20f7fc67
jonasromano <88295040+jonasromano@users.noreply.github.com>
2021-10-31 09:12:25 -0300

    f

--- js/my-profile.js
+++ js/my-profile.js
@@ -12,11 +12,62 @@ function profile(){
     let perfil = JSON.stringify(datos);
     localStorage.setItem('perfile',perfil);
 }
-function alerta(){
-    
-}
+
 
 
 document.addEventListener("DOMContentLoaded", function (e) {
+    document.getElementById("registro");("click", function(){
+        let Nombre = document.getElementById("name");
+        let Apellido = document.getElementById("subname");
+        let Contraseña = document.getElementById("inputPassword4");
+        let Correo = document.getElementById("inputEmail4");
+        let Telefono = document.getElementById("phone");
+        let camposCompletos = true; 
+ 
+        if(Nombre.value === " "){
+            Nombre.classList.add("invalid");
+            camposCompletos = false;
+        }else{
+            Nombre.classList.remove("invalid");
+        }
+        if(Apellido.value === " "){
+            Apellido.classList.add("invalid");
+            camposCompletos = false;
+        }else{
+            Apellido.classList.remove("invalid");
+        }
+        if(Contraseña.value === " "){
+            Contraseña.classList.add("invalid");
+            camposCompletos = false;
+        }else{
+            Contraseña.classList.remove("invalid");
+        }
+        if(Correo.value === " "){
+            Correo.classList.add("invalid");
+            camposCompletos = false;
+        }else{
+            Correo.classList.remove("invalid");
+        }
+        if(Telefono.value === " "){
+            Telefono.classList.add("invalid");
+            camposCompletos = false;
+        }else{
+            Telefono.classList.remove("invalid");
+        }
+        if(camposCompletos){
+            getJSONData().then(function(resultado){
+                if(resultado.status === "ok"){
+                    usersArray = resultado.data;
+                    if(validateUser(usersArray,Nombre.value,Apellido.value,Contraseña.value,Correo.value,Telefono.value)){
+                        window.location = 'my-profile.html';
+                    }else{
+                        alert("Usuario o contraseña incorrectas!");
+                    }
+                }
+            });
+        }else{
+            alert("Debes ingresar los datos!")
+        }
+    });
 
 });
