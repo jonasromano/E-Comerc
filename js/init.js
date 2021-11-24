2ee7e4cbbcd0251aadb2289c72f6b16cf4d2de67
jonasromano <88295040+jonasromano@users.noreply.github.com>
2021-11-24 20:17:51 -0300

    8

--- js/init.js
+++ js/init.js
@@ -1,13 +1,13 @@
-const CATEGORIES_URL = "http://localhost:3000/category";
-const PUBLISH_PRODUCT_URL = "http://localhost:3000/publish";
-const CATEGORY_INFO_URL = "http://localhost:3000/cat_info";
-const PRODUCTS_URL = "http://localhost:3000/productos";
-const PRODUCT_INFO_URL = "http://localhost:3000/productos_info";
-const PRODUCT_INFO_COMMENTS_URL = "http://localhost:3000/productos_coment";
-const CART_INFO_URL = "http://localhost:3000/cart_info";
-const CART_BUY_URL = "http://localhost:3000/cart_buy";
-const NEW_PRODUCTS_URL="http://localhost:3000/new_productos";
-const NEW_CART_URL="http://localhost:3000/new_cart"
+const CATEGORIES_URL = "https://japdevdep.github.io/ecommerce-api/category/all.json";
+const PUBLISH_PRODUCT_URL = "https://japdevdep.github.io/ecommerce-api/product/publish.json";
+const CATEGORY_INFO_URL = "https://japdevdep.github.io/ecommerce-api/category/1234.json";
+const PRODUCTS_URL = "https://japdevdep.github.io/ecommerce-api/product/all.json";
+const PRODUCT_INFO_URL = "https://japdevdep.github.io/ecommerce-api/product/5678.json";
+const PRODUCT_INFO_COMMENTS_URL = "https://japdevdep.github.io/ecommerce-api/product/5678-comments.json";
+const CART_INFO_URL = "https://japdevdep.github.io/ecommerce-api/cart/987.json";
+const CART_BUY_URL = "https://japdevdep.github.io/ecommerce-api/cart/buy.json";
+const NEW_PRODUCTS_URL="https://jonasromano.github.io/E-Comerc/productos.JSON";
+const NEW_CART_URL="https://japdevdep.github.io/ecommerce-api/cart/654.json"
 
 var showSpinner = function(){
   document.getElementById("spinner-wrapper").style.display = "block";
