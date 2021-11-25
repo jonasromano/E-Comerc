69a09f16988aacbc07694611cb2571751f4a85a6
jonasromano <88295040+jonasromano@users.noreply.github.com>
2021-11-25 18:19:03 -0300

    8

--- js/init.js
+++ js/init.js
@@ -9,6 +9,18 @@ const CART_BUY_URL = "https://japdevdep.github.io/ecommerce-api/cart/buy.json";
 const NEW_PRODUCTS_URL="https://jonasromano.github.io/E-Comerc/productos.JSON";
 const NEW_CART_URL="https://japdevdep.github.io/ecommerce-api/cart/654.json"
 
+
+//const CATEGORIES_URL = "http://localhost:3000/category";
+//const PUBLISH_PRODUCT_URL = "http://localhost:3000/publish";
+//const CATEGORY_INFO_URL = "http://localhost:3000/cat_info";
+//const PRODUCTS_URL = "http://localhost:3000/productos";
+//const PRODUCT_INFO_URL = "http://localhost:3000/productos_info";
+//const PRODUCT_INFO_COMMENTS_URL = "http://localhost:3000/productos_coment";
+//const CART_INFO_URL = "http://localhost:3000/cart_info";
+//const CART_BUY_URL = "http://localhost:3000/cart_buy";
+//const NEW_PRODUCTS_URL="http://localhost:3000/new_productos";
+//const NEW_CART_URL="http://localhost:3000/new_cart"
+
 var showSpinner = function(){
   document.getElementById("spinner-wrapper").style.display = "block";
 }
